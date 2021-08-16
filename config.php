30ab6c8994d0c82d0eab56eba1d53aa8b668bf9d
Gokul R <52248324+Gokul-R-13@users.noreply.github.com>
2021-08-16 13:18:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,6 @@
 	$database = "bank";
 	// Create a database connection
 	$conn = mysqli_connect($server, $username, $password, $database);
-	//$conn = mysqli_connect('shareddb-x.hosting.stackcp.net','shivangi','12345678@','malyaraaj-313537e38d');
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
